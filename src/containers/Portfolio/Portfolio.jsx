cfe2f5c7f268e179025939228831133eae6f396b
Tara Martin <martin.tara117@gmail.com>
2021-02-06 11:08:52 -0500

    added links to portfolio

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -18,43 +18,43 @@ const Portfolio = () => {
             image={Insp}
             title="The Inspirator"
             description="The Inspirator is an application to help unlock creativity for persons needing a fresh approach. This site provides a different perspective to help continue or initiate a project, from writing a song to plotting a story. "
-            github="https://github.com/dnfrancis22/GT-Project1"
-            deployedSite="https://dnfrancis22.github.io/GT-Project1/"
+            github="https://github.com/martintara117/GT-Project1"
+            deployedSite="https://martintara117.github.io/GT-Project1/"
           />
           <Card
             image={Tungsten}
-            title="Food Truck Chasers"
-            description="Food truck chasers is an application where users in a city can chase all the food trucks in their area by searching by zip code and food category. For food truck operators it is a application to help drive traffic to their food trucks and create an awareness of where they are on any given day."
-            github="https://github.com/dnfrancis22/FoodTruck-Chasers"
-            deployedSite="https://glacial-ocean-57671.herokuapp.com/"
+            title="Tungsten Tools"
+            description="Tungsten Tools is an application where users can rent tools from other people in their area by searching by zip code and tool category."
+            github="https://github.com/martintara117/project-tungsten"
+            deployedSite="https://floating-crag-67869.herokuapp.com/"
           />
           <Card
             image={Wa}
             title="The Weather Dashboard"
-            description="This is a weather app. You can search for a city and get the current weather as well as a five forecast. The users search will be generated into a button that can be clicked and the weather information for that city will be recalled."
-            github="https://github.com/dnfrancis22/weather-dashboard"
-            deployedSite="https://dnfrancis22.github.io/weather-dashboard/"
+            description="This weather app allows the user to search for a city and get a five day weather forecast."
+            github="https://github.com/martintara117/weather_api"
+            deployedSite="https://martintara117.github.io/weather_api/"
           />
           <Card
             image={Workout}
-            title="The Work Day Scheduler"
-            description="This is a daily planner created to give busy workers a way to manage their day."
-            github="https://github.com/dnfrancis22/work-day-planner"
-            deployedSite="https://dnfrancis22.github.io/work-day-planner/"
+            title="Fitness Tracker"
+            description="This workout app lets users track and save their daily workouts. It has a stats page that shows users the progress in graphs and tables."
+            github="https://github.com/martintara117/workout_tracker"
+            deployedSite="https://git.heroku.com/mighty-brushlands-74381.git"
           />
           <Card
             image={Burger}
             title="The Eat Da Burger App"
-            description="This app simply allows you to create a burger, and devour the burger."
-            github="https://github.com/dnfrancis22/burger"
-            deployedSite="https://polar-brook-84844.herokuapp.com/"
+            description="This app lets users create a burger and then consume the burger."
+            github="https://github.com/martintara117/burger"
+            deployedSite="https://salty-plains-85711.herokuapp.com/"
           />
           <Card
             image={Directory}
             title="Employee Directory"
-            description="This app allows you to view your employee directory. You can sort by name ascending or descending. You can also filter by number."
-            github="https://github.com/dnfrancis22/employee-directory"
-            deployedSite="https://dnfrancis22.github.io/employee-directory/"
+            description="This app allows you to search for people in the employee directory. Type their first or last name and the employee will appear."
+            github="https://github.com/martintara117/employee_directory-"
+            deployedSite="https://martintara117.github.io/employee_directory-/"
           />
         </div>
       </div>
